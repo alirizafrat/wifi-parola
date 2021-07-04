9965816c115ba12ecbe28211857a14690eb02f04
Ali Rıza Fırat <79906043+alirizafrat@users.noreply.github.com>
2021-07-04 13:47:34 +0300

    Add files via upload

---
+++ wifi.py
@@ -0,0 +1,26 @@
+import subprocess
+
+data = (
+    subprocess.check_output(["netsh", "wlan", "show", "profiles"])
+    .decode("utf-8")
+    .split("\n")
+)
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
+    results = (
+        subprocess
+        .check_output(["netsh", "wlan", "show", "profile", i, "key=clear"])
+        .decode("utf-8")
+        .split("\n")
+    )
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    try:
+        print("{:<30}|  {:<}".format(i, results[0]))
+    except IndexError:
+        print("{:<30}|  {:<}".format(i, ""))
+
+input("bir tuşa basınız")
+
+###   Bu Proje Ali Rıza Fırat tarafından eğitim amaçlı olarak paylaşılmıştır.
+###   https://alirizafirat.com/
+###   alirizafirat@aol.com
